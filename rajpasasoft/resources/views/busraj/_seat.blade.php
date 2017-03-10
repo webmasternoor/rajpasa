0671edef35ca1b30ce6d7c821a9e7f390e0e10c3
Noor <webmaster.noor@gmail.com>
2017-03-10 16:38:53 +0600

    Seat Booking updated
    Seat Booking updated

--- rajpasasoft/resources/views/busraj/_seat.blade.php
+++ rajpasasoft/resources/views/busraj/_seat.blade.php
@@ -1,90 +1,162 @@
-Bus ID: {{$temp}}
-<div class="form-group">
-    <div class="col-md-6 col-md-push-3">
-        <a href="javascript:ajaxLoad('busraj/list')" class="btn btn-danger"><i
-                    class="glyphicon glyphicon-backward"></i>
-            Back</a>
+<div class="col-md-12">
+    <div class="modal-header">
+        <h4 class="modal-title" id="H1">Choose your seat(s)</h4>
     </div>
-</div>
-<div class="col-md-12 seatarra">                        
-    <table>
-        <tr>                                
-            <td>
-                <div>
-                <p class="driverimg"><img src="./images/driver.png" alt=""></p>
-<?php
-/*$table_name = 'seatbuses';
-$columns = DB::select('show columns from ' . $table_name);
-foreach ($columns as $value) {
-   echo $value->Field ;
-}
-*/
-//$users = Seatbus::table('seatbuses')->where('bus_id',$temp)->get();
-//echo $us = Seatbus::seatbuse()->total_seat;
+    <div class="col-md-6">
+        Bus ID: {{$temp}}
+        <?php
+        $total_seat = DB::table('busrajs')->where('bus_id',$temp)->get();
+        foreach ($total_seat as $er)
+        {   
+            echo "<br/>Seat Fare: ".$er->seat_fare;
+            echo "<br/>Bus type: ".$er->bus_type;
+        }
+        ?>
+        <div class="form-group">
+            <div class="col-md-6 col-md-push-3">
+                <a href="javascript:ajaxLoad('busraj/list')" class="btn btn-danger"><i
+                            class="glyphicon glyphicon-backward"></i>
+                    Back</a>
+            </div>
+        </div>
+        <div class="col-md-12 seatarra">                        
+            <table>
+                <tr>                                
+                    <td>
+                        <div>
+                        <p class="driverimg"><img src="./images/driver.png" alt=""></p>
+        <?php
+        /*$table_name = 'seatbuses';
+        $columns = DB::select('show columns from ' . $table_name);
+        foreach ($columns as $value) {
+           echo $value->Field ;
+        }
+        */
+        //$users = Seatbus::table('seatbuses')->where('bus_id',$temp)->get();
+        //echo $us = Seatbus::seatbuse()->total_seat;
 
-//Auth::user()->type == 'admin')
-$total_seat = DB::table('busrajs')->where('bus_id',$temp)->get();
-foreach ($total_seat as $er)
-{   
-    //echo $er->total_seat.".";
-}
-$users = DB::table('seatbuses')->where('bus_id',$temp)->get();
-foreach ($users as $user)
-{
-    //$table_name = 'seatbuses';
-    //$columns = DB::select('show columns from ' . $table_name);
-    
-    //foreach ($columns as $value) {
-    //$tte = $value->Field ;
-    //echo $user->id;
-    for($i=0;$i<$er->total_seat;$i++){
-        //echo $user->$i;
-        echo "<p id='$i' value='$i' class='blue' onclick='selected_seat();'>&nbsp;</p>";
-        
-    }    
-    ?>
-
-    <p id="show_seat"></p>
-    <?php
-    //echo "<br/>";
-    //echo $user->$tte;
-
-    /*if($user->$tte == '0'){
-    ?>
-    
-                <p class="blue"><?php //echo $user->$tte; ?> &nbsp;</p>
-    <?php
-    }*/
-    //echo $value->Field;
-    //echo "<br/>";
-    //}
-    //echo $user->1;
-    //echo $user->10;
-    //echo mysql_field_name($user, 0) . "\n";
-    //echo mysql_field_name($user, 2);
-}
-?>
-                </div>
-            </td>
-        </tr>
-    </table>                        
+        //Auth::user()->type == 'admin')
+        $total_seat = DB::table('busrajs')->where('bus_id',$temp)->get();
+        foreach ($total_seat as $er)
+        {   
+            //echo $er->seat_fare;
+        }
+        $users = DB::table('seatbuses')->where('bus_id',$temp)->get();
+        foreach ($users as $user)
+        {
+            //$table_name = 'seatbuses';
+            //$columns = DB::select('show columns from ' . $table_name);
+            
+            //foreach ($columns as $value) {
+            //$tte = $value->Field ;
+            //echo $user->id;
+            $counter = 0;
+            for($i=1;$i<$er->total_seat;$i++){
+                //echo $user->$i;
+                echo "<input type='button' id='$i' value='$i' class='blue' onclick='selected_seat();' />";
+                //echo $counter."<br/>";
+                $counter++;
+                if($i%4 == 0)
+                {
+                    echo "<br/>";
+                }
+                
+            }    
+            ?>
+        <!-- <input type="button" id="button2" value="Display" onclick="display_array();"></input> -->
+            <p id="show_seat"></p>
+            <?php
+        }
+        ?>
+                        </div>
+                    </td>
+                </tr>
+            </table>                        
+        </div>
+    </div>
+    <div class="col-md-6">
+        <div class="row">
+            <div class="col-sm-4" style="padding-right:0;">
+                <ul id="Ul71" class="list-inline clearfix">
+                    <li class="seat-white"></li>
+                    <li class="legend-label">Available Seats</li>
+                </ul>
+            </div>
+            <div class="col-sm-4" style="padding-right:0;">
+                <ul id="Ul72" class="list-inline clearfix">
+                    <li class="seat-gray"></li>
+                    <li class="legend-label">Booked Seats</li>
+                </ul>
+            </div>
+            <div class="col-sm-4" style="padding-right:0;">
+                <ul id="Ul73" class="list-inline clearfix">
+                    <li class="seat-green"></li>
+                    <li class="legend-label">Selected Seats</li>
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div id="tbl_price_details">
+                <table class="table-responsive">
+                    <thead>
+                        <tr>
+                            <th>Seats</th>
+                            <th>Fare</th>
+                            <th>Class</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <div id="Result"></div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>                           
+        </div>
+    </div>
 </div> 
 <script>
-$( "p" ).click(function() {
+$( "input" ).click(function() {
   $( this ).toggleClass( "highlight" );
 });
 </script>
 <script type="text/javascript">
     var total_selected_seat = 0;
-    //document.getElementById('') = 
-
+    var x = 0;
+    var array = Array();
     function selected_seat(){
-    //document.write('i am here');
+     array[x] = document.getElementById(selected_seat.caller.arguments[0].target.id).value;
+     //alert("Element: " + array[x] + " Added at index " + x);
+     x++;
+
+     //var e = "<hr/>";   
+     var e = "";   
     
-    //document.write(++total_selected_seat);
-    document.getElementById("show_seat").innerHTML = ++total_selected_seat;
-    
-    //setText("show_seat",++total_selected_seat);
+   for (var y=0; y<array.length; y++)
+   {
+     //e += "Element " + y + " = " + array[y] + "<br/>";
+     e += array[y] + "<br/>";
+     //var e = e.unique();
+   }
+   document.getElementById("Result").innerHTML = e;
+
 }
+/*function display_array()
+{
+   var e = "<hr/>";   
+    
+   for (var y=0; y<array.length; y++)
+   {
+     //e += "Element " + y + " = " + array[y] + "<br/>";
+     e += array[y] + ",";
+     //var e = e.unique();
+   }
+   document.getElementById("Result").innerHTML = e;
+}*/
+</script>
+<script type="text/javascript">
+    
 </script>
   <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
